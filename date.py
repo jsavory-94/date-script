d6d21823fa852d12a167fb240a1b29b5077ba531
James Savory <james.pa.savory@gmail.com>
2020-05-22 11:01:59 -0400

    printing example.py foo

--- date.py
+++ date.py
@@ -6,10 +6,11 @@ def get_dates(yr, mo, day):
    date_str = f'{yr}-{mo}-{day}'
    date = dt.datetime.strptime(date_str, '%Y-%m-%d')
    weekday = date.weekday()
-   print(weekday)
+   #print(weekday)
 
 
    if weekday != 0:
+      print('non-monday code running')
       date_previous = date - timedelta(days=7)
       date_previous_str = date_previous.strftime('%Y-%m-%d')
       date_previous = dt.datetime.strptime(date_previous_str, '%Y-%m-%d')
@@ -30,14 +31,14 @@ def get_dates(yr, mo, day):
 
       while weekday_1 > 0:
          #print(weekday_decrementor)
-         print('finding previous monday to time given')
+         #print('finding previous monday to time given')
          date_dict_new = {'current': date - timedelta(days=weekday_decrementor), 'previous': date_previous - timedelta(days=weekday_decrementor)}
-         print(date_dict_new)
-         print(date_dict_new['previous'])
-         print(type(date_dict_new['previous']))
+         #print(date_dict_new)
+         ##print(date_dict_new['previous'])
+         #print(type(date_dict_new['previous']))
          # print(date_dict_new)
          master_list.append(date_dict_new)
-         print(f'current weekday {weekday_1}')
+         #print(f'current weekday {weekday_1}')
          weekday_1 -= 1
          weekday_decrementor +=1
 
@@ -47,17 +48,24 @@ def get_dates(yr, mo, day):
 
       while weekday_2 < 6:
          #print(weekday_decrementor)
-         print('finding next Sunday to time given')
+         #print('finding next Sunday to time given')
          date_dict_new_1 = {'current': date + timedelta(days=weekday_incrementor), 'previous': date_previous + timedelta(days=weekday_incrementor)}
-         print(date_dict_new_1)
+         #print(date_dict_new_1)
          # print(date_dict_new)
          master_list.append(date_dict_new_1)
-         print(f'current weekday {weekday_2}')
+         #print(f'current weekday {weekday_2}')
          weekday_2 += 1
          weekday_incrementor +=1
 
-      print(master_list)
-      return master_list
+      # print(master_list)
+
+      master_list_sorted = sorted(master_list, key=lambda i: i['current'])
+
+      print('---printed---')
+      print(master_list_sorted)
+
+      print('---returned---')
+      return master_list_sorted
 
 
    else:
